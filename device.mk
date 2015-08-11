1b1b498856e038c5e83df54591f9038f1ae77250
Manikanta Sivapala <msivap@codeaurora.org>
2015-08-11 18:12:08 +0530

    common.mk: adding compilation of libextmedia_jni & libqcmediaplayer
    adding compilation of libextmedia_jni & libqcmediaplayer
    
    Change-Id: I6d7cefccdd9c05031d3e323817acb8003b36f5d2

--- device.mk
+++ device.mk
@@ -138,7 +138,9 @@ PRODUCT_PACKAGES += \
     libOmxVdecHevc \
     libOmxVenc \
     libstagefrighthw \
-    qcmediaplayer
+    libqcmediaplayer \
+    qcmediaplayer \
+    libextmedia_jni
 
 PRODUCT_BOOT_JARS += \
     qcmediaplayer
